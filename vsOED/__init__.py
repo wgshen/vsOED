c2f8b5a226d04cf8995013155098fc782995f5fa
wgshen <wgshen@umich.edu>
2023-04-30 14:46:36 -0400

    update init

--- vsOED/__init__.py
+++ vsOED/__init__.py
@@ -1,6 +1,7 @@
 from .vsoed import VSOED
 from .pg_vsoed import PGvsOED
+from .post_approx import GMM_NET, NFs, POST_APPROX
 
 __all__ = [
-    "VSOED", "PGvsOED"
+    "VSOED", "PGvsOED", "GMM_NET", "NFs", "POST_APPROX"
 ]
