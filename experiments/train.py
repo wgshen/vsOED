8cf9d7aa60baa961494252f171e536d0efe37b97
wgshen <wgshen@umich.edu>
2023-05-02 16:33:13 -0400

    update experiments

---
+++ experiments/train.py
@@ -0,0 +1,298 @@
+import os,sys,inspect
+# current_dir = '/home/jiayuand/seqOED_variational/examples/location'
+# parent_dir = '/home/jiayuand/seqOED_variational'
+
+# current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# parent_dir = os.path.dirname(os.path.dirname(current_dir))
+parent_dir = '/scratch/xhuan_root/xhuan1/wgshen/vsOED/'
+sys.path.insert(0, parent_dir) 
+
+import numpy as np
+# from scipy.stats import norm, beta, dirichlet
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from   torch.utils.data import DataLoader, TensorDataset
+import torch.optim as optim
+if torch.cuda.is_available():  
+    dev = "cuda:0" 
+else:  
+    dev = "cpu"  
+device = torch.device(dev) 
+torch.set_default_device(device)
+dtype = torch.float32
+torch.set_default_dtype(dtype)
+
+from vsOED import VSOED, PGvsOED, GMM_NET, NFs, POST_APPROX
+from vsOED.utils import *
+from vsOED.models import *
+
+import dowel
+
+n_stage      = 5       # Number of stages.
+n_design     = 2        # Number of design variables.
+n_obs        = 1        # Number of observations.
+n_pois       = [4]        # Number of parameters.
+n_nuisps     = None
+n_goals      = None
+model_weight = 0
+poi_weight   = 1
+goal_weight  = 0
+
+random_state  = TRAIN_SEEDS[0]
+set_random_seed(random_state)
+
+model = SOURCE(multimodel=False, include_nuisance=False, include_goal=False)
+prior = model
+
+mu_bounds = {}
+mu_bounds['poi'] = []
+for n_poi in n_pois:
+    mu_bounds['poi'].append([[-6, 6]] * n_poi)
+if n_goals is not None:
+    mu_bounds['goal'] = []
+    for n_goal in n_goals:
+        mu_bounds['goal'].append([[0, 3]] * n_goal)
+max_sigmas = {}
+max_sigmas['poi'] = []
+for n_poi in n_pois:
+    max_sigmas['poi'].append([1.0] * n_poi)
+if n_goals is not None:
+    max_sigmas['goal'] = []
+    for n_goal in n_goals:
+        max_sigmas['goal'].append([1.0] * n_goal)
+truncnorm_info = {}
+truncnorm_info['poi'] = [None] * len(n_pois)
+if n_goals is not None:
+    truncnorm_info['goal'] = []
+    for n_goal in n_goals:
+        info = []
+        for i in range(n_goal):
+            info.append([i, 0, 4])
+            
+n_incre = 1
+share_interm_net = False
+
+candidate_lrs = [[1e-3] * n_incre, [1e-4] * (n_incre - 1) + [1e-3]]
+candidate_gammas = [[0.9999] * n_incre, [0.999999] * (n_incre - 1) + [0.9999]]
+model_post_lrs = candidate_lrs[share_interm_net]
+model_post_lr_scheduler_gammas = candidate_gammas[share_interm_net]
+poi_post_lrs = candidate_lrs[share_interm_net]
+poi_post_lr_scheduler_gammas = candidate_gammas[share_interm_net]
+goal_post_lrs = candidate_lrs[share_interm_net]
+goal_post_lr_scheduler_gammas = candidate_gammas[share_interm_net]
+
+use_NFs = False
+n_mixture = 8
+activate = nn.ReLU
+
+log_every = 100
+folder = parent_dir + f'experiments/source_{len(n_pois)}model_{n_stage}stage_{n_incre}incre_GMM/'
+try:
+    os.remove(folder + 'progress.csv')
+except:
+    pass
+try:
+    os.remove(folder + 'progress.txt')
+except:
+    pass
+try:
+    dowel.logger.remove_all()
+except:
+    pass
+dowel.logger.add_output(dowel.StdOutput())
+dowel.logger.add_output(dowel.CsvOutput(folder + 'progress.csv'))
+dowel.logger.add_output(dowel.TextOutput(folder + 'progress.txt'))
+
+try:
+    for _ in range(100):
+        dowel.logger.pop_prefix()
+except:
+    pass
+dowel.logger.push_prefix('[SOURCE] ')
+dowel.logger.log('Stored at ' + folder)
+dowel.logger.log('Random seed:  ' + str(random_state))
+dowel.logger.log('Device: ' + str(device))
+dowel.logger.log('dtype: ' + str(dtype))
+
+post_approx_params = {
+    'n_stage': n_stage, 
+    'n_design': n_design, 
+    'n_obs': n_obs, 
+    'n_pois': n_pois,
+    'n_nuisps': n_nuisps,
+    'n_goals': n_goals,
+    'model_weight': model_weight,
+    'poi_weight': poi_weight,
+    'goal_weight': goal_weight,
+    'mu_bounds': mu_bounds,
+    'max_sigmas': max_sigmas,
+    'truncnorm_info': truncnorm_info,
+    'n_mixture': n_mixture,
+    'activate': activate,
+    'prior': prior,
+    'n_incre': n_incre,
+    'share_interm_net': share_interm_net,
+    'model_post_lrs': model_post_lrs,
+    'model_post_lr_scheduler_gammas': model_post_lr_scheduler_gammas,
+    'poi_post_lrs': poi_post_lrs,
+    'poi_post_lr_scheduler_gammas': poi_post_lr_scheduler_gammas,
+    'goal_post_lrs': goal_post_lrs,
+    'goal_post_lr_scheduler_gammas': goal_post_lr_scheduler_gammas,
+    'use_NFs': use_NFs,
+    'log_every': log_every,
+    'dowel': dowel}
+
+dowel.logger.log('Post_approx_params: ' + str(post_approx_params))
+
+dowel.logger.dump_all()
+# dowel.logger.remove_all()
+
+post_approx = POST_APPROX(**post_approx_params)
+dowel.logger.dump_all()
+
+n_param = len(n_pois) > 1
+try:
+    n_param += max(n_pois)
+except:
+    pass
+try:
+    n_param += max(n_nuisps)
+except:
+    pass
+try:
+    n_param += max(n_goals)
+except:
+    pass
+
+design_bounds = [(-4, 4), (-4, 4)] # lower and upper bounds of design variables.
+def reward_fun(*args, **kws):
+    return 0
+nkld_reward_fun = reward_fun
+kld_reward_fun = post_approx.reward_fun
+phys_state_info = None
+post_approx = post_approx
+encoder_dimns = None
+backend_dimns = None
+actor_dimns = [256, 256, 256]
+critic_dimns = [256, 256, 256]
+
+vsoed_params = {
+    'n_stage': n_stage,
+    'n_param': n_param,
+    'n_design': n_design,
+    'n_obs': n_obs,
+    'model': model,
+    'prior': prior,
+    'design_bounds': design_bounds,
+    'nkld_reward_fun': nkld_reward_fun,
+    'kld_reward_fun': kld_reward_fun,
+    'phys_state_info': phys_state_info,
+    'post_approx': post_approx,
+    'encoder_dimns': encoder_dimns,
+    'backend_dimns': backend_dimns,
+    'actor_dimns': actor_dimns,
+    'critic_dimns': critic_dimns,
+    'activate': activate
+}
+dowel.logger.log('vsoed_params: ' + str(vsoed_params))
+
+vsoed = PGvsOED(**vsoed_params)
+
+actor_lr = 1e-3
+actor_lr_scheduler_gamma = 0.9999
+critic_lr = 1e-3
+critic_lr_scheduler_gamma = 0.9999
+    
+n_update = 1001
+n_newtraj = 1000
+n_batch = 10000
+n_buffer_init = n_batch * 2
+n_buffer_max = int(1e6)
+buffer_device = torch.device('cuda')
+discount = 1 if n_incre == 1 else 0.9
+encoder_actor_optimizer = None
+encoder_actor_lr_scheduler = None
+encoder_critic_optimizer = None
+encoder_critic_lr_scheduler = None
+actor_optimizer = optim.Adam(vsoed.actor_net.parameters(), lr=actor_lr)
+actor_lr_scheduler = optim.lr_scheduler.ExponentialLR(actor_optimizer, gamma=actor_lr_scheduler_gamma)
+n_critic_update = 5
+critic_optimizer = optim.Adam(vsoed.critic_net.parameters(), lr=critic_lr)
+critic_lr_scheduler = optim.lr_scheduler.ExponentialLR(critic_optimizer, gamma=critic_lr_scheduler_gamma)
+n_post_approx_update = 5
+lr_target = 0.1
+design_noise_scale = 0.5
+design_noise_decay = 0.9999
+on_policy = False
+use_PCE = False
+use_PCE_incre = None
+n_contrastive_sample = None
+transition = 10000 if n_incre == 1 else 0
+frozen = -1
+save_every = 100
+save_path = folder
+restart = False
+
+vsoed_train_params = {
+    'n_update': n_update,
+    'n_newtraj': n_newtraj,
+    'n_batch': n_batch,
+    'n_buffer_init': n_buffer_init,
+    'n_buffer_max': n_buffer_max,
+    'buffer_device': buffer_device,
+    'discount': discount,
+    'encoder_actor_optimizer': encoder_actor_optimizer,
+    'encoder_actor_lr_scheduler': encoder_actor_lr_scheduler,
+    'encoder_critic_optimizer': encoder_critic_optimizer,
+    'encoder_critic_lr_scheduler': encoder_critic_lr_scheduler,
+    'actor_lr': actor_lr,
+    'actor_optimizer': actor_optimizer,
+    'actor_lr_scheduler_gamma': actor_lr_scheduler_gamma, 
+    'actor_lr_scheduler': actor_lr_scheduler,
+    'n_critic_update': n_critic_update,
+    'critic_lr': critic_lr,
+    'critic_optimizer': critic_optimizer,
+    'critic_lr_scheduler_gamma': critic_lr_scheduler_gamma,
+    'critic_lr_scheduler': critic_lr_scheduler,
+    'n_post_approx_update': n_post_approx_update,
+    'lr_target': lr_target,
+    'design_noise_scale': design_noise_scale,
+    'design_noise_decay': design_noise_decay,
+    'on_policy': on_policy,
+    'use_PCE': use_PCE,
+    'use_PCE_incre': use_PCE_incre,
+    'n_contrastive_sample': n_contrastive_sample,
+    'transition': transition,
+    'frozen': frozen,
+    'log_every': log_every,
+    'dowel': dowel,
+    'save_every': save_every,
+    'save_path': save_path,
+    'restart': restart
+}
+
+dowel.logger.log('vsoed_train_params: ' + str(vsoed_train_params))
+
+del vsoed_train_params['actor_lr']
+del vsoed_train_params['actor_lr_scheduler_gamma']
+del vsoed_train_params['critic_lr']
+del vsoed_train_params['critic_lr_scheduler_gamma']
+
+
+try:
+    for _ in range(100):
+        dowel.logger.pop_prefix()
+except:
+    pass
+dowel.logger.push_prefix('[SOURCE] ')
+vsoed.train(**vsoed_train_params)
+
+set_random_seed(EVAL_SEEDS[0])
+ret = vsoed.asses(2000, n_contrastive_sample=int(1e6), return_all=True, dowel=dowel, save_path=folder + 'evaluation.pt')
+# averaged_reward = ret['averaged_reward']
+# params = ret['params']
+# ds_hist = ret['ds_hist']
+# ys_hist = ret['ys_hist']
+# rewards_hist = ret['rewards_hist']
+# averaged_reward
