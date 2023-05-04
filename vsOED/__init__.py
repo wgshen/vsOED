c5138a5c36d3966044a73df46180b090eb78ca02
Jiayuan Dong <jiayuand@gl-login2.arc-ts.umich.edu>
2023-05-04 18:02:12 -0400

    update from jiayuand

--- vsOED/__init__.py
+++ vsOED/__init__.py
@@ -1,7 +1,9 @@
 from .vsoed import VSOED
 from .pg_vsoed import PGvsOED
 from .post_approx import GMM_NET, NFs, POST_APPROX
+from .post_approx_NFs import POST_APPROX_NF        ####### delete this part when there is no problem
+
 
 __all__ = [
-    "VSOED", "PGvsOED", "GMM_NET", "NFs", "POST_APPROX"
+    "VSOED", "PGvsOED", "GMM_NET", "NFs", "POST_APPROX",  "POST_APPROX_NF"
 ]
